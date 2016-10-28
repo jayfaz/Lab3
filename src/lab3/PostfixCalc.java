609a68b37f445fe1837f8829e9f07d0ef0a5aa4d
fazioj7 <fazioj7@RobiLab312-03.rowanads.rowan.edu>
2016-10-27 21:05:59 -0400

    Take 2

--- src/lab3/PostfixCalc.java
+++ src/lab3/PostfixCalc.java
@@ -12,11 +12,34 @@ public class PostfixCalc {
 		
 		int num1;
 		int num2;
+		int num3;
 		
 		for(int i = 0; i < values.length; i++) {
 			if(values[i].equals("+")) {
 				num1 = stack.pop();
+				num2 = stack.pop();
+				num3 = num1 + num2;
+				stack.push(num3);
+			} else if(values[i].equals("-")) {
+				num1 = stack.pop();
+				num2 = stack.pop();
+				num3 = num2 - num1;
+				stack.push(num3);
+			} else if(values[i].equals("*")) {
+				num1 = stack.pop();
+				num2 = stack.pop();
+				num3 = num1 * num2;
+				stack.push(num3);
+			} else if(values[i].equals("/")) {
+				num1 = stack.pop();
+				num2 = stack.pop();
+				num3 = num2 / num1;
+				stack.push(num3);
+			} else {
+				stack.push(Integer.parseInt(values[i]));
 			}
+			
+			
 		}
 	}
 
